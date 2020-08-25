ea9215d846a6b2ba669d9792e8b26461fefa9834
Thilan Tran <thilanoftran@gmail.com>
2020-08-24 21:30:41 -0700

    Add new interactive scripts

---
+++ static/life.js
@@ -0,0 +1,148 @@
+const RESOLUTION = 10;
+const LIFESPAN = 5;
+const DELAY = 80;
+const X_BOUND = Math.floor(window.innerWidth / RESOLUTION);
+const Y_BOUND = Math.floor(window.innerHeight / RESOLUTION);
+
+const PATTERNS = {
+  acorn: [
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [1, 1, 0, 0, 1, 1, 1]
+  ],
+  rPentomino: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 1, 0]
+  ],
+  dieHard: [
+    [0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 1, 1]
+  ]
+};
+
+let GRID = [];
+let GRID_CPY = [];
+for (let i = 0; i < X_BOUND; i++) {
+  const row = [];
+  const rowCpy = [];
+  for (let j = 0; j < Y_BOUND; j++) {
+    row.push({ live: false, spawned: false, kill: () => {} });
+    rowCpy.push({ live: false, spawned: false, kill: () => {} });
+  }
+  GRID.push(row);
+  GRID_CPY.push(rowCpy);
+}
+
+const randRange = (range) => Math.floor(Math.random() * range);
+
+const createCell = (container, i, j, force = false) => {
+  if (i >= GRID.length || j >= GRID[0].length) return;
+
+  const element = GRID[i][j];
+  const cpy = GRID_CPY[i][j];
+  if (!element.live) {
+    const part = document.createElement('div');
+    part.classList.add('particle');
+    part.style.left = i * RESOLUTION + 'px';
+    part.style.top = j * RESOLUTION + 'px';
+    container.appendChild(part);
+    element.live = true;
+    element.lifespan = (1000 * LIFESPAN) / DELAY;
+    element.kill = () => {
+      part.parentNode.removeChild(part);
+      element.live = false;
+    };
+    if (force) cpy.live = true;
+  }
+};
+
+const spawnPattern = (pattern) => {
+  const container = document.querySelector('.container');
+  const randX = randRange(parseInt(X_BOUND * 0.9)) + parseInt(X_BOUND * 0.1);
+  const randY = randRange(parseInt(Y_BOUND * 0.9)) + parseInt(Y_BOUND * 0.1);
+  pattern.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      if (cell === 1) {
+        createCell(container, randX + i, randY + j, true);
+      }
+    });
+  });
+};
+
+const clickClosestCell = (x, y) => {
+  const idxX = Math.floor(x / RESOLUTION);
+  const idxY = Math.floor(y / RESOLUTION);
+  const container = document.querySelector('.container');
+  createCell(container, idxX, idxY, true);
+};
+
+const getWrapped = (ref, i, j) => {
+  const xBound = ref.length;
+  const yBound = ref[0].length;
+  let newI = i,
+    newJ = j;
+
+  if (i < 0) newI = xBound - 1;
+  else if (i >= xBound) newI = 0;
+  if (j < 0) newJ = yBound - 1;
+  else if (j >= yBound) newJ = 0;
+  return ref[newI][newJ];
+};
+
+const countNeighbors = (ref, i, j) => {
+  let cnt = 0;
+  if (getWrapped(ref, i - 1, j - 1).live) cnt++;
+  if (getWrapped(ref, i - 1, j).live) cnt++;
+  if (getWrapped(ref, i - 1, j + 1).live) cnt++;
+
+  if (getWrapped(ref, i, j - 1).live) cnt++;
+  if (cnt > 3) return cnt;
+  if (getWrapped(ref, i, j + 1).live) cnt++;
+  if (cnt > 3) return cnt;
+
+  if (getWrapped(ref, i + 1, j - 1).live) cnt++;
+  if (cnt > 3) return cnt;
+  if (getWrapped(ref, i + 1, j).live) cnt++;
+  if (cnt > 3) return cnt;
+  if (getWrapped(ref, i + 1, j + 1).live) cnt++;
+  return cnt;
+};
+
+const draw = () => {
+  const container = document.querySelector('.container');
+  GRID.forEach((row, i) => {
+    row.forEach((elem, j) => {
+      const numNeighbors = countNeighbors(GRID_CPY, i, j);
+      if (elem.live) {
+        if (numNeighbors < 2 || numNeighbors > 3 || elem.lifespan <= 0) {
+          elem.kill();
+        }
+        elem.lifespan--;
+      } else {
+        if (numNeighbors === 3) {
+          createCell(container, i, j);
+        }
+      }
+    });
+  });
+  GRID.forEach((row, i) => {
+    row.forEach((elem, j) => {
+      GRID_CPY[i][j].live = elem.live;
+    });
+  });
+};
+
+document.querySelector('.title').addEventListener('click', (evt) => {
+  const pats = Object.values(PATTERNS);
+  const idx = randRange(pats.length);
+  spawnPattern(pats[idx]);
+  evt.target.blur();
+});
+
+// document.addEventListener('click', (evt) => {
+//   clickClosestCell(evt.clientX, evt.clientY);
+// });
+
+setInterval(draw, DELAY);
---
+++ static/particles.js
@@ -0,0 +1,63 @@
+const MAX_SPEED = 3;
+const MAX_LIFESPAN = 12;
+const DELAY = 5;
+
+const randRange = (range) => Math.floor(Math.random() * range);
+const getRandSpeed = () =>
+  (Math.random() > 0.5 ? 1 : -1) * (randRange(MAX_SPEED) + 1);
+const getRandLife = () => randRange((MAX_LIFESPAN * 1000) / DELAY) + 1;
+
+const addParticle = (x, y) => {
+  const part = document.createElement('div');
+  part.classList.add('particle');
+  part.style.left = x + 'px';
+  part.style.bottom = y + 'px';
+  part.dataset.velX = getRandSpeed();
+  part.dataset.velY = getRandSpeed();
+  part.dataset.lifetime = getRandLife();
+  document.querySelector('.container').appendChild(part);
+};
+
+const moveParticles = () => {
+  document.querySelectorAll('.particle').forEach((part) => {
+    const life = Number(part.dataset.lifetime);
+    const x = parseInt(part.style.left);
+    const y = parseInt(part.style.bottom);
+    const velX = Number(part.dataset.velX);
+    const velY = Number(part.dataset.velY);
+
+    if (part.dataset.lifetime === '0') {
+      part.parentNode.removeChild(part);
+      return;
+    } else {
+      part.dataset.lifetime = life - 1;
+    }
+
+    let newX = x + velX;
+    let newY = y + velY;
+    if (newX > window.innerWidth) newX = 0;
+    if (newX < 0) newX = window.innerWidth;
+    if (newY > window.innerHeight) newY = 0;
+    if (newY < 0) newY = window.innerHeight;
+    part.style.left = newX + 'px';
+    part.style.bottom = newY + 'px';
+  });
+};
+
+document.querySelector('.title').addEventListener('click', (evt) => {
+  evt.target.blur();
+});
+
+document.addEventListener('click', (evt) => {
+  addParticle(evt.clientX, window.innerHeight - evt.clientY);
+});
+
+// document.querySelector('.title').addEventListener('mousedown', () => {
+//   const x = randRange(window.innerWidth);
+//   const y = randRange(window.innerHeight);
+//   addParticle(x, y);
+// });
+
+setInterval(() => {
+  moveParticles();
+}, DELAY);
