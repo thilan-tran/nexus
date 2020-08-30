91716ccb2ec2122041c04545f10595497c046fe9
Thilan Tran <thilanoftran@gmail.com>
2020-08-29 23:42:16 -0700

    more ios specific detetion

--- static/main.js
+++ static/main.js
@@ -286,7 +286,6 @@ const iOS = () =>
     'iPhone',
     'iPod'
   ].includes(navigator.platform) ||
-  // iPad on iOS 13 detection
   (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
 
 if (iOS) {
